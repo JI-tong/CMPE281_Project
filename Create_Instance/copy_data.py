211605513b8da5152a129817e468c390f3646b7c
Eric <ton9eric1@gmail.com>
2018-05-08 19:55:31 -0700

    copy_data
    1

--- Create_Instance/copy_data.py
+++ Create_Instance/copy_data.py
@@ -9,10 +9,10 @@ DATABASE = 'labelme.db'
 con = sql.connect(DATABASE)
 cur = con.cursor()
 
-cur.execute("SELECT dns from users")
+cur.execute("SELECT dns,id from users")
 dns = cur.fetchall()
 
 
 key_path = "./Key_Pairs/MutipleUse.pem"
 for user_dns in dns:
-    os.system('scp -i {} -r ubuntu@{}:~/test_folder ~/{}'.format(key_path, user_dns, user_dns) )
+    os.system('scp -i {} -r ubuntu@{}:~/test_folder ~/{}'.format(key_path, user_dns[0], user_dns[1]))
--- Create_Instance/server.py
+++ Create_Instance/server.py
@@ -125,5 +125,5 @@ if __name__ == '__main__':
 	# app.run(host='0.0.0.0', port=80)
 	# then log in using ec2 instance public ip
     start_db()
-    #app.run(debug = True)
-    app.run(host='0.0.0.0', port=80)
+    app.run(debug = True)
+    #app.run(host='0.0.0.0', port=80)
